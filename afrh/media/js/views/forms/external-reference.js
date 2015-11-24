93341594972ca5d135d9a6a5f54865249569da36
adam cox <mr.adamcox@gmail.com>
2015-11-23 20:56:10 -0600

    adding new templates/js

---
+++ afrh/media/js/views/forms/external-reference.js
@@ -0,0 +1,30 @@
+define(['jquery',
+    'views/forms/base',
+    'views/forms/sections/branch-list'
+    ], function ($, BaseForm, BranchList) {
+    return BaseForm.extend({
+        initialize: function() {
+            BaseForm.prototype.initialize.apply(this);
+
+            this.addBranchList(new BranchList({
+                el: this.$el.find('#xref-section')[0],
+                data: this.data,
+                dataKey: 'EXTERNAL_RESOURCE.E1',
+                validateBranch: function(nodes){
+                    return this.validateHasValues(nodes);
+                },
+                isUrl: function(value) {
+                    console.log(value);
+                    return /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/.test(value);
+                },
+                getLink: function(value) {
+                    if (/^https?:\/\//.test(value)) {
+                        return value;
+                    } else {
+                        return 'http://' + value;
+                    }
+                }
+            }));
+        }
+    });
+});
