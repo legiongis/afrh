3224fde1000406592bcd72440406f628a1b9925b
adam cox <mr.adamcox@gmail.com>
2016-01-13 15:55:18 -0600

    changes to favicon, index photo captions, character area overlay layers... (messy commit!)

--- afrh/media/js/map/layers.js
+++ afrh/media/js/map/layers.js
@@ -27,6 +27,36 @@ define([
         })
     });
     
+    var char_area_lyr_group = new ol.layer.Group({
+        ///style: 'AerialWithLabels',
+        layers: [
+            new ol.layer.Tile({
+                name: 'char_areas',
+                source: new ol.source.TileWMS({
+                    url: 'http://afrh.adamcfcox.com/geoserver/zonemaps/wms/',
+                    params: {
+                        'LAYERS': 'zonemaps:char_areas_map2',
+                        'TILED': true,
+                    },
+                    serverType: 'geoserver'
+                }),
+            }),
+            new ol.layer.Tile({
+                name: 'char_areas',
+                source: new ol.source.TileWMS({
+                    url: 'http://afrh.adamcfcox.com/geoserver/zonemaps/wms/',
+                    params: {
+                        'LAYERS': 'zonemaps:circ_character_area',
+                        'TILED': true,
+                    },
+                    serverType: 'geoserver'
+                }),
+            }),
+        ],
+        visible: false,
+        is_arches_layer: "nobutclose",
+    });
+    
     var char_areas = new LayerModel({
         name: 'Character Areas',
         categories: ["Reference"],
@@ -34,18 +64,7 @@ define([
         infoContent: "Showing the character areas at the AFRH.<br><b>The following Character Areas are <em>not</em> shown here:</b> Circulation System, Recurring Resources, and Spatial Patterns.",
         onMap: true,
         active: false,
-        layer: new ol.layer.Tile({
-            visible: false,
-            is_arches_layer: "nobutclose",
-            source: new ol.source.TileWMS({
-                url: 'http://afrh.adamcfcox.com/geoserver/zonemaps/wms/',
-                params: {
-                    'LAYERS': 'zonemaps:char_areas_map',
-                    'TILED': true,
-                },
-                serverType: 'geoserver'
-            })
-        })
+        layer: char_area_lyr_group,
     });
     
     var mp_zones = new LayerModel({
