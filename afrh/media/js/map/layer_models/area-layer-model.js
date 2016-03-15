7d1190c66b692bb55b31c7e651d6730a9aa48893
Adam Cox <mr.adamcox@gmail.com>
2016-03-15 16:41:55 -0600

    create layer models for new resource layer display options

---
+++ afrh/media/js/map/layer_models/area-layer-model.js
@@ -0,0 +1,98 @@
+define([
+    'jquery',
+    'openlayers',
+    'underscore',
+    'arches',
+    'map/layer-model',
+    'utils'
+], function($, ol, _, arches, LayerModel, utils) {
+    return function(config, featureCallback) {
+        config = _.extend({
+            entitytypeid: 'all',
+            vectorColor: '#808080'
+        }, config);
+
+        var layer = function () {
+            var fillrgb = utils.hexToRgb(config.fillColor);
+            var strokergb = utils.hexToRgb(config.strokeColor);
+            var zIndex = 0;
+            var styleCache = {};
+
+            var polyStyle = function(feature, resolution) {
+                var mouseOver = feature.get('mouseover');
+                var text = '1 ' + mouseOver;
+
+                if (!feature.get('arches_marker')) {
+                    feature.set('arches_marker', true);
+                }
+
+                if (styleCache[text]) {
+                    return styleCache[text];
+                }
+                
+                var iconSize = mouseOver ? 38 : 32;
+
+                var styles = [new ol.style.Style({
+                    stroke: new ol.style.Stroke({
+                        color: 'rgba(' + strokergb.r + ',' + strokergb.g + ',' + strokergb.b + ',0.25)',
+                        width: 10
+                    }),
+                    //fill: new ol.style.Fill({
+                    //    color: 'rgba(' + fillrgb.r + ',' + fillrgb.g + ',' + fillrgb.b + ',0.45)'
+                    //}),
+                    zIndex: mouseOver ? zIndex*1000000000: zIndex
+                }),new ol.style.Style({
+                    stroke: new ol.style.Stroke({
+                        color: 'rgba(' + strokergb.r + ',' + strokergb.g + ',' + strokergb.b + ',0.9)',
+                        width: 4
+                    }),
+                    //fill: new ol.style.Fill({
+                    //    color: 'rgba(' + fillrgb.r + ',' + fillrgb.g + ',' + fillrgb.b + ',0.45)'
+                    //}),
+                    zIndex: mouseOver ? zIndex*2000000000: zIndex
+                })
+                ];
+                
+                zIndex += 2;
+
+                styleCache[text] = styles;
+                return styles;
+            };
+
+            var layerConfig = {
+                projection: 'EPSG:3857'
+            };
+            
+            if (config.entitytypeid !== null) {
+                layerConfig.url = arches.urls.polygon_layers + config.entitytypeid;
+            }
+
+            json_source = new ol.source.GeoJSON(layerConfig)
+            
+            $('.map-loading').show();
+            var loadListener = json_source.on('change', function(e) {
+                if (json_source.getState() == 'ready') {
+                    if(typeof(featureCallback) === 'function'){
+                        featureCallback(json_source.getFeatures());
+                    }
+                    ol.Observable.unByKey(loadListener);
+                    $('.map-loading').hide();
+                }
+            });
+            
+            output_layer = new ol.layer.Vector({
+                source: json_source,
+                style: polyStyle,
+                visible: config.entitytypeid == "INVENTORY_RESOURCE.E18" 
+            });
+            return output_layer
+        };
+        
+        return new LayerModel(_.extend({
+                layer: layer,
+                onMap: true,
+                isArchesLayer: true
+            }, config)
+        );
+    };
+});
