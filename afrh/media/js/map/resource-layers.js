fe94781ad206e80c9c22f6d0fb8296ebcee45f5f
Adam Cox <mr.adamcox@gmail.com>
2016-03-15 17:10:50 -0600

    define ALL resource layers here using new layer models

--- afrh/media/js/map/resource-layers.js
+++ afrh/media/js/map/resource-layers.js
@@ -4,14 +4,45 @@ define([
         'knockout',
         'arches',
         'resource-layer-info',
-        'map/resource-layer-model'
-], function(ol, _, ko, arches, resourceLayerInfo, ResourceLayerModel) {
+        'map/resource-layer-model',
+        'map/layer_models/marker-layer-model',
+        'map/layer_models/full-layer-model',
+        'map/layer_models/polygon-layer-model',
+        'map/layer_models/area-layer-model',
+        'resource-types',
+        'user-info'
+
+], function(ol, _, ko, arches, resourceLayerInfo, ResourceLayerModel, MarkerLayerModel, FullLayerModel, PolygonLayerModel, AreaLayerModel, resourceTypes, userInfo) {
         var resourceFeatures = ko.observableArray();
         var layers = [];
-
-        _.each(resourceLayerInfo, function (item, entitytypeid) {
+        
+        var layerModelMap = {
+            'area':AreaLayerModel,
+            'poly':PolygonLayerModel,
+            'marker':MarkerLayerModel,
+            //'project_areas':ActivityLayerModel
+        }
+        
+        _.each(resourceTypes, function (item, entitytypeid) {
+            
+            //exclude if not mappy resource
+            if (item.makeLayer == false){return true;}
+            
+            //exclude based on user permissions
+            if (userInfo.name == 'anonymous' && item.restricted){return true;}
+            
             item.entitytypeid = entitytypeid;
-            layers.push(new ResourceLayerModel(item, function(features) {
+            item.onMap = true;
+            
+            item.active = false;
+            if (entitytypeid == "INVENTORY_RESOURCE.E18"){
+                item.active = true;
+            }
+
+            //select which layer model to use based on resource type layer_model value
+            var newLayerModel = layerModelMap[item.layerModel];
+
+            layers.push(new newLayerModel(item, function(features) {
                 resourceFeatures(resourceFeatures().concat(features));
             }));
         });
