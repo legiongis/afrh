4c10c1600e2aec29e48ac427ed92856f1b8c8308
Adam Cox <mr.adamcox@gmail.com>
2016-04-11 12:19:55 -0600

    reconfigure permission hierarchy, delete all HIP permissions

--- afrh/setup.py
+++ afrh/setup.py
@@ -62,11 +62,21 @@ def create_database_users():
 def build_auth_system():
     '''builds all default AFRH-specific permissions, groups, and users'''
     
+    print "\nREMOVING ARCHES-HIP PERMISSIONS & GROUPS\n-----------------------"
+    all_perms = Permission.objects.filter()
+    for p in all_perms:
+        p.delete()
+    print "  {} permissions removed".format(len(all_perms))
+    
+    all_groups = Group.objects.filter()
+    for g in all_groups:
+        g.delete()
+    print "  {} groups removed".format(len(all_groups))
+    
     print "\nBUILDING AFRH AUTH SYSTEM\n-----------------------"
 
-    ## using blank model for the content type, as described in this post
+    ## using blank model for the content type, as described in this post, maybe not a good long-term solution
     ## http://stackoverflow.com/questions/13932774/how-can-i-use-django-permissions-without-defining-a-content-type-or-model
-    ## not a good long-term solution
     
     print "  creating groups...",
     group_dict = {
@@ -78,192 +88,6 @@ def build_auth_system():
     print "done."
 
     print "  creating permissions and adding to groups...",
-    # permissions = {
-        # 'INVENTORY_RESOURCE':{
-            # 'name':'Inventory Resource',
-            # 'create':[admin1,admin2],
-            # 'edit':[admin1,admin2],
-            # 'fullreport':[admin1,admin2,afrh_staff],
-            # 'view':[public,admin1,admin2,afrh_staff,development],
-            # },
-        # 'MASTER_PLAN_ZONE':{
-            # 'name':'Master Plan Zone',
-            # 'create':[admin1,admin2],
-            # 'edit':[admin1,admin2],
-            # 'fullreport':[public,admin1,admin2,afrh_staff,development],
-            # 'view':[public,admin1,admin2,afrh_staff,development],
-            # },
-        # 'CHARACTER_AREA':{
-            # 'name':'Character Area',
-            # 'create':[admin1,admin2],
-            # 'edit':[admin1,admin2],
-            # 'fullreport':[public,admin1,admin2,afrh_staff,development],
-            # 'view':[public,admin1,admin2,afrh_staff,development],
-            # },
-        # 'ARCHAEOLOGICAL_ZONE':{
-            # 'name':'Archaeological Zone',
-            # 'create':[admin1,admin2],
-            # 'edit':[admin1,admin2],
-            # 'fullreport':[admin1,admin2,afrh_staff],
-            # 'view':[public,admin1,admin2,afrh_staff,development],
-            # },
-        # 'DESIGNATION':{
-            # 'name':'Designation',
-            # 'create':[admin1,admin2],
-            # 'edit':[admin1,admin2],
-            # 'fullreport':[public,admin1,admin2,afrh_staff,development],
-            # 'view':[public,admin1,admin2,afrh_staff,development],
-            # },
-        # 'MANAGEMENT_ACTIVITY_A':{
-            # 'name':'Management Activity A',
-            # 'create':[admin1,admin2,afrh_staff],
-            # 'edit':[admin1,admin2,afrh_staff],
-            # 'fullreport':[admin1,admin2,afrh_staff],
-            # 'view':[admin1,admin2,afrh_staff],
-            # },
-        # 'MANAGEMENT_ACTIVITY_B':{
-            # 'name':'Management Activity A',
-            # 'create':[admin1,admin2,development],
-            # 'edit':[admin1,admin2,development],
-            # 'fullreport':[admin1,admin2,afrh_staff,development],
-            # 'view':[admin1,admin2,afrh_staff,development],
-            # }
-        # }
-    
-    for res, info in settings.RESOURCE_TYPE_CONFIGS().iteritems():
-        cd,nm = res.split(".")[0],info['name']
-        content_type = ContentType.objects.get_or_create(app_label=nm, model=cd)
-        # create = Permission.objects.create(codename='create', name='Create', content_type=content_type[0])
-        # edit = Permission.objects.create(codename='edit', name='Edit', content_type=content_type[0])
-        # fullreport = Permission.objects.create(codename='fullreport', name='Full Report', content_type=content_type[0])
-        # view = Permission.objects.create(codename='view', name='View', content_type=content_type[0])
-        
-        perm_dict = {
-            'create':Permission.objects.create(codename='create', name='Create', content_type=content_type[0]),
-            'edit':Permission.objects.create(codename='edit', name='Edit', content_type=content_type[0]),
-            'fullreport':Permission.objects.create(codename='fullreport', name='Full Report', content_type=content_type[0]),
-            'view':Permission.objects.create(codename='view', name='View', content_type=content_type[0])
-        }
-        
-        for type, groups in info['permissions'].iteritems():
-            if groups == 'all':
-                for grp in group_dict.values():
-                    grp.permissions.add(perm_dict[type])
-                continue
-            for group in groups:
-                group_dict[group].permissions.add(perm_dict[type])
-        
-    
-    # for res,info in permissions.iteritems():
-        
-        # cd,nm = res, info['name']
-        # content_type = ContentType.objects.get_or_create(app_label=nm, model=cd)
-        # create = Permission.objects.create(codename='create', name='Create', content_type=content_type[0])
-        # edit = Permission.objects.create(codename='edit', name='Edit', content_type=content_type[0])
-        # fullreport = Permission.objects.create(codename='fullreport', name='Full Report', content_type=content_type[0])
-        # view = Permission.objects.create(codename='view', name='View', content_type=content_type[0])
-        # for k,v in info.iteritems():
-            # if k == 'create':
-                # for grp in v:
-                    # grp.permissions.add(create)
-            # if k == 'edit':
-                # for grp in v:
-                    # grp.permissions.add(edit)
-            # if k == 'fullreport':
-                # for grp in v:
-                    # grp.permissions.add(fullreport)
-            # if k == 'view':
-                # for grp in v:
-                    # grp.permissions.add(view)
-    
-    content_type = ContentType.objects.get_or_create(app_label="AFRH", model="afrh")
-    rdm = Permission.objects.create(codename='rdm_access', name='RDM Access', content_type=content_type[0])
-    group_dict['admin1'].permissions.add(rdm)
-    print "done."
-    
-    print "  creating users...",
-    admin1_user = User.objects.create_user("admin1","","pw")
-    admin1_user.groups.add(group_dict['admin1'])
-    admin2_user = User.objects.create_user("admin2","","pw")
-    admin2_user.groups.add(group_dict['admin2'])
-    afrh_staff_user = User.objects.create_user("afrh_staff","","pw")
-    afrh_staff_user.groups.add(group_dict['afrh_staff'])
-    development_user = User.objects.create_user("development","","pw")
-    development_user.groups.add(group_dict['development'])
-    print "done."
-    print ""
-
-def build_auth_system_2():
-    '''builds all default AFRH-specific permissions, groups, and users'''
-    
-    print "\nBUILDING AFRH AUTH SYSTEM\n-----------------------"
-
-    ## using blank model for the content type, as described in this post
-    ## http://stackoverflow.com/questions/13932774/how-can-i-use-django-permissions-without-defining-a-content-type-or-model
-    ## not a good long-term solution
-    
-    print "  creating groups...",
-    group_dict = {
-        'admin1':Group.objects.get_or_create(name='Admin 1')[0],
-        'admin2':Group.objects.get_or_create(name='Admin 2')[0],
-        'afrh_staff':Group.objects.get_or_create(name='AFRH - Staff')[0],
-        'development':Group.objects.get_or_create(name='Development')[0],
-    }
-    print "done."
-
-    print "  creating permissions and adding to groups...",
-    # permissions = {
-        # 'INVENTORY_RESOURCE':{
-            # 'name':'Inventory Resource',
-            # 'create':[admin1,admin2],
-            # 'edit':[admin1,admin2],
-            # 'fullreport':[admin1,admin2,afrh_staff],
-            # 'view':[public,admin1,admin2,afrh_staff,development],
-            # },
-        # 'MASTER_PLAN_ZONE':{
-            # 'name':'Master Plan Zone',
-            # 'create':[admin1,admin2],
-            # 'edit':[admin1,admin2],
-            # 'fullreport':[public,admin1,admin2,afrh_staff,development],
-            # 'view':[public,admin1,admin2,afrh_staff,development],
-            # },
-        # 'CHARACTER_AREA':{
-            # 'name':'Character Area',
-            # 'create':[admin1,admin2],
-            # 'edit':[admin1,admin2],
-            # 'fullreport':[public,admin1,admin2,afrh_staff,development],
-            # 'view':[public,admin1,admin2,afrh_staff,development],
-            # },
-        # 'ARCHAEOLOGICAL_ZONE':{
-            # 'name':'Archaeological Zone',
-            # 'create':[admin1,admin2],
-            # 'edit':[admin1,admin2],
-            # 'fullreport':[admin1,admin2,afrh_staff],
-            # 'view':[public,admin1,admin2,afrh_staff,development],
-            # },
-        # 'DESIGNATION':{
-            # 'name':'Designation',
-            # 'create':[admin1,admin2],
-            # 'edit':[admin1,admin2],
-            # 'fullreport':[public,admin1,admin2,afrh_staff,development],
-            # 'view':[public,admin1,admin2,afrh_staff,development],
-            # },
-        # 'MANAGEMENT_ACTIVITY_A':{
-            # 'name':'Management Activity A',
-            # 'create':[admin1,admin2,afrh_staff],
-            # 'edit':[admin1,admin2,afrh_staff],
-            # 'fullreport':[admin1,admin2,afrh_staff],
-            # 'view':[admin1,admin2,afrh_staff],
-            # },
-        # 'MANAGEMENT_ACTIVITY_B':{
-            # 'name':'Management Activity A',
-            # 'create':[admin1,admin2,development],
-            # 'edit':[admin1,admin2,development],
-            # 'fullreport':[admin1,admin2,afrh_staff,development],
-            # 'view':[admin1,admin2,afrh_staff,development],
-            # }
-        # }
-    
     perm_types = ['create','edit','fullreport','view']
     
     for perm in perm_types:
@@ -280,45 +104,7 @@ def build_auth_system_2():
                         continue
                     for group in groups:
                         group_dict[group].permissions.add(this_perm)
-                    
 
-            # perm_dict = {
-                # 'create':Permission.objects.create(codename='create', name='Create', content_type=content_type[0]),
-                # 'edit':Permission.objects.create(codename='edit', name='Edit', content_type=content_type[0]),
-                # 'fullreport':Permission.objects.create(codename='fullreport', name='Full Report', content_type=content_type[0]),
-                # 'view':Permission.objects.create(codename='view', name='View', content_type=content_type[0])
-            # }
-            
-            
-                # if groups == 'all':
-                    
-                    # continue
-                # for group in groups:
-                    # group_dict[group].permissions.add(perm_dict[type])
-            
-    
-    # for res,info in permissions.iteritems():
-        
-        # cd,nm = res, info['name']
-        # content_type = ContentType.objects.get_or_create(app_label=nm, model=cd)
-        # create = Permission.objects.create(codename='create', name='Create', content_type=content_type[0])
-        # edit = Permission.objects.create(codename='edit', name='Edit', content_type=content_type[0])
-        # fullreport = Permission.objects.create(codename='fullreport', name='Full Report', content_type=content_type[0])
-        # view = Permission.objects.create(codename='view', name='View', content_type=content_type[0])
-        # for k,v in info.iteritems():
-            # if k == 'create':
-                # for grp in v:
-                    # grp.permissions.add(create)
-            # if k == 'edit':
-                # for grp in v:
-                    # grp.permissions.add(edit)
-            # if k == 'fullreport':
-                # for grp in v:
-                    # grp.permissions.add(fullreport)
-            # if k == 'view':
-                # for grp in v:
-                    # grp.permissions.add(view)
-    
     content_type = ContentType.objects.get_or_create(app_label="AFRH", model="afrh")
     rdm = Permission.objects.create(codename='rdm_access', name='RDM Access', content_type=content_type[0])
     group_dict['admin1'].permissions.add(rdm)
