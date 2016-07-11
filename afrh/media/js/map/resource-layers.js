301ea968bd318bf1bdc01f00a09dae12f2cfea58
Adam Cox <mr.adamcox@gmail.com>
2016-07-11 15:37:41 -0500

    remove obsolete field investigations map layer (now included with the arch-zone layer)

--- afrh/media/js/map/resource-layers.js
+++ afrh/media/js/map/resource-layers.js
@@ -10,11 +10,10 @@ define([
         'map/layer_models/polygon-layer-model',
         'map/layer_models/area-layer-model',
         'map/layer_models/arch-layer-model',
-        'map/layer_models/arch-investigation-layer-model',
         'resource-types',
         'user-info'
 
-], function(ol, _, ko, arches, resourceLayerInfo, ResourceLayerModel, MarkerLayerModel, FullLayerModel, PolygonLayerModel, AreaLayerModel, ArchLayerModel, ArchInvestigationModel, resourceTypes, userInfo) {
+], function(ol, _, ko, arches, resourceLayerInfo, ResourceLayerModel, MarkerLayerModel, FullLayerModel, PolygonLayerModel, AreaLayerModel, ArchLayerModel, resourceTypes, userInfo) {
         var resourceFeatures = ko.observableArray();
         var layers = [];
         
@@ -48,43 +47,13 @@ define([
             var newLayerModel = layerModelMap[item.layerModel];
             
             if (entitytypeid == "ARCHAEOLOGICAL_ZONE.E53"){
-                item.infoContent = "this is the content";
+                item.infoContent = true;
             }
 
             layers.push(new newLayerModel(item, function(features) {
                 resourceFeatures(resourceFeatures().concat(features));
             }));
             
-            if (entitytypeid == "ARCHAEOLOGICAL_ZONE.E53"){
-                item.infoContent = "this is the content";
-                arch_invest_item = {
-                    'active':false,
-                    'color':"#f600f1",
-                    'defaultDescription':"no description available",
-                    'description':"this is description property speaking",
-                    'descriptionNode':"ACTIVITY_SCOPE_OF_WORK_DESCRIPTION.E62",
-                    'entitytypeid':"ACTIVITY_B.E7",
-                    'fillColor':"#c8b2a3",
-                    'icon':"fa fa-clipboard",
-                    'iconColor':"#f600f1",
-                    'id':"ARCHEOLOGICAL_ZONE_INVESTIGATION",
-                    'infoContent':"",
-                    'layerModel':"marker",
-                    'makeLayer':true,
-                    'name':"Field Investigations",
-                    'onMap':true,
-                    'restricted':true,
-                    'strokeColor':"#a7673d",
-                    'vectorColor':"#a44b0f"
-                }
-
-                var arch_invest_layer = new ArchInvestigationModel(
-                    arch_invest_item, function(features) {
-                    resourceFeatures(resourceFeatures().concat(features));
-                });
-
-                layers.push(arch_invest_layer);
-            }
         });
         
         
